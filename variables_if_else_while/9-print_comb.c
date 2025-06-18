9462c8605bedaa310d9f780a9fdc40993953409d
Priam <priamdmy@gmail.com>
2025-06-18 17:58:05 +0200

    added 9-print_comb.c, compiled and tested

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+ 
+		if (i >= 49 && i <= 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
