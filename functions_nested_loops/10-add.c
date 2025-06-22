8665bdf04d63e7d1e265e218656d3fa3c3bf675b
Priam <priamdmy@gmail.com>
2025-06-22 22:12:09 +0200

    added 10-add.c, compiled and tested successfully locally

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: First value
+ * @b: Second value
+ *
+ * Return: sum of integers
+ */
+
+int add(int a, int b)
+{
+	int sum = (a + b);
+
+	return (sum);
+}
