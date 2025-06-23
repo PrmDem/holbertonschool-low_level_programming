36212cab522dcb2f088a6c57339fc258cda7ef1d
Priam <priamdmy@gmail.com>
2025-06-23 10:29:58 +0200

    added 2-mul.c, compiled and tested locally, test successful

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first value
+ * @b: second value
+ *
+ * Return: value of mul
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = (a * b);
+
+	return (sum);
+}
