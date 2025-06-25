fd0202f838a4238cb50333d87a090bdda2fb0923
Priam <priamdmy@gmail.com>
2025-06-25 17:13:11 +0200

    compiled and tested 8-print_array locally, successful

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array
+ * in order, separated by a comma and a space
+ * @a: pointer to array
+ * @n: number of elements in the array
+ *
+ * Return: none (void)
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+			printf("%d\n", a[i]);
+
+		else
+			printf("%d, ", a[i]);
+	}
+}
