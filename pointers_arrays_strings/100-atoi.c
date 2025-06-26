a92102dfca00052089acb310e5c5744958a8c4d3
Priam <priamdmy@gmail.com>
2025-06-26 22:09:10 +0200

    coded, compiled, and tested locally, successful

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an int
+ * @s: points to string to convert
+ *
+ * Return: int (numbers within s)
+ */
+
+int _atoi(char *s)
+{
+	int len = 0, min = 0, count = 0, num = 0;
+
+	while (s[len] != '\0')
+	{
+		if ((s[len] < 48 || s[len] > 57) && count > 0)
+		{
+		break;
+		}
+
+		if (s[len] == 45)
+		{
+			min++;
+		}
+
+		if (s[len] >= 48 && s[len] <= 57)
+		{
+			num = num * 10 + (s[len] - 48);
+			count++;
+		}
+
+		len++;
+	}
+
+	if (min % 2 != 0)
+		num = num * (-1);
+
+	return (num);
+}
