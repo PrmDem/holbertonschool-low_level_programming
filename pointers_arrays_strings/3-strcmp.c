32072b421fbcad2d7d1fba66922188b1e50fc519
Priam <priamdmy@gmail.com>
2025-06-27 16:55:47 +0200

    v2 moved position of return

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int res = (s1[i] - s2[i]);
+	int l = 0;
+	int ln = 0;
+	int res;
 
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[l] != '\0' || s2[ln] != '\0')
 	{
+		res = s1[l] - s2[ln];
+
 		if (res == 0)
-			i++;
+		{
+			l++;
+			ln++;
+		}
 
 		else
+		{
 			break;
+		}
 	}
 
 	return (res);
