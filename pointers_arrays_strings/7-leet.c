29e4575f00f7f22460937ae7a7262f351a79dc58
Priam <priamdmy@gmail.com>
2025-06-29 22:49:07 +0200

    compiled and tested 7-leet, successful after correct break implementation; 2 while 1 if

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * leet - encodes string into 1337
+ * @str: string to encode
+ *
+ * Return: char
+ */
+
+char *leet(char *str)
+{
+	char base[6] = "oleat";
+	char leet[6] = "01347";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		int j = 0;
+		
+		while (j < 5)
+		{
+			if (str[i] == base[j] || str[i] == (base[j] - 32))
+			{
+			       	str[i] = leet[j];
+				break;
+        		}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
