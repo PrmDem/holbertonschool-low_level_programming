f0488a4806c94440959daf377d5231e462997ec6
Priam <priamdmy@gmail.com>
2025-06-30 16:00:12 +0200

    compiled and tested locally, with return NULL and return != NULL; successful

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strchr - looks for a given char in string
+ * @s: string to search in
+ * @c: character to look for
+ *
+ * Return: pointer to first occurrence of c if found
+ * NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int found = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			s = &s[i];
+			found++;
+			break;
+		}
+
+		else
+		{
+			i++;
+		}
+	}
+
+	if (found > 0)
+		return (s);
+
+	else
+		return ("NULL");
+}
