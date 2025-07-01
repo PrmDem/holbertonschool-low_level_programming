2a8415b4f8e65a88367ffb82f685ad22efa76b4b
Priam <priamdmy@gmail.com>
2025-07-01 14:46:44 +0200

    tested and edited until successful, compiled

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: ref to the number of arrays total
+ * @8: number of elements per array
+ *
+ * return: none (void)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x = 0;
+	int y;
+
+	while (x < 8)
+	{
+		y = 0;
+		
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+
+		_putchar('\n');
+		x++;
+	}
+	
+}
