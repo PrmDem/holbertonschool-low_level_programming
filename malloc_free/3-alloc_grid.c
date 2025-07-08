60d8f7158c3f17d2ef15ba89315c7c7f38bab43f
Priam <priamdmy@gmail.com>
2025-07-08 11:13:36 +0200

    first push after successful test

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - returns pointer to 2d array
+ * @width: columns
+ * @height: rows
+ *
+ * Return: pointer to array, or
+ * NULL if fail or any arg <= 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid == NULL)
+			return (NULL);
+
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
