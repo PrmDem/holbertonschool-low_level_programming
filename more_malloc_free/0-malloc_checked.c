e9fc50988a2a1c8f394d4011f032828c305b0bf0
Priam <priamdmy@gmail.com>
2025-07-09 10:59:29 +0200

    [first] compil & test successful locally

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* malloc_checked - allocates memory
+* @b: unsigned int to allocate to
+*
+* Return: pointer to allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *mall;
+
+	mall = malloc(b);
+
+	if (mall == NULL)
+		exit (98);
+	
+	return (mall);
+}
