9239684f612417c2b56e0fd6b031686cd249abeb
Priam <priamdmy@gmail.com>
2025-07-10 10:36:14 +0200

    [fix] (size * nmemb) -> (nmemb * sizeof(size))

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,12 +12,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *array;
+	int *array;
+	unsigned int i;
 
-	array = malloc(size * nmemb);
-
-	if (array == NULL || nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
+
+	array = malloc(nmemb * sizeof(size));
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		array[i] = 0;
 
 	return (array);
 }
