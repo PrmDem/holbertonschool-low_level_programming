4a0b1e495ffd4267a8bbe6682afb1c2db25bb6cc
Priam <priamdmy@gmail.com>
2025-07-11 09:40:15 +0200

    [upd] fully rewritten for clarity

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,53 +5,52 @@
 * string_nconcat - concatenates two strings
 * @s1: first string
 * @s2: second string
-* @n: # of s2 bytes to concatenate
+* @n: bytes of s2 to use
 *
-* Return: pointer to allocated mem
-* or NULL on failure.
+* Return: pointer to start of string
+* or NULL if failure, s1 or s2 == 0
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *string;
-	unsigned int i, j, size1, size2;
-	size1 = size2 = i = j = 0;
+    unsigned int size1, size2, i, j;
+    size1 = size2 = i = j = 0;
+    char *string;
 
-	if (s1 == NULL)
-		s1 = "";
+    if (s1 == NULL)
+        s1 = "";
 
-	if (s2 == NULL)
-		s2 = "";
+    if (s2 == NULL)
+        s2 = "";
 
-	while (s1[size1] != '\0')
-	{
-		size1++;
-	}
+    while (s1[size1] != '\0')
+    {
+        size1++;
+    }
 
-	while (s2[size2] != '\0')
-	{
-		size2++;
-	}
+    while (s2[size2] != '\0')
+    {
+        size2++;
+    }
 
-	if (n > size2)
-			n = size2;
+    if (n >= size2)
+        n = size2;
 
-	string = malloc(sizeof(char) * (size1 * size2));
+    string = malloc(size1 + n + 1);
+    if (string == NULL)
+        return (NULL);
 
-	if (string == NULL)
-		return (NULL);
+    for (i = 0; i < size1; i++)
+    {
+        string[i] = s1[i];
+    }
 
-	for (i = 0; i < size1; i++)
-	{
-		string[i] = s1[i];
-	}
+    for (j = 0; j < n; j++)
+    {
+        string[i + j] = s2[j];
+    }
 
-	for (j = 0; j < n; j++)
-	{
-		string[i + j] = s2[j];
-	}
+    string[i + j] = '\0';
 
-	string[i + j] = '\0';
-
-	return (string);
+    return (string);
 }
