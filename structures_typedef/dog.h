d25d33f8e4a7cc44ef75d90c9e4b6ebe01d0bd8a
Priam <priamdmy@gmail.com>
2025-07-11 14:31:02 +0200

    first push

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - for our dog facts
+* @name: dog's name
+* @age: dog's age
+* @owner: owner's name
+*
+* Contains name, age, and owner
+* of any dog, big or small
+*/
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
