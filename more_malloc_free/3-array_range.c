3569123face3e3d2150043dcd18458d321ab025b
Priam <priamdmy@gmail.com>
2025-07-11 11:17:42 +0200

    [betty] indent changed to tabs

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -2,27 +2,36 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+* array_range - allocates memory to array
+* @min: min end of range
+* @max: max end of range
+*
+* Return: pointer to memory
+* or NULL if min > max or malloc fail
+*/
+
 int *array_range(int min, int max)
 {
-    int *range;
+	int *range;
 	int count = 0;
-    int i = 0;
+	int i = 0;
 
-    if (min > max)
-        return (NULL);
+	if (min > max)
+		return (NULL);
 
-    range = malloc(sizeof(int) * (max - min + 1));
-    if (range == NULL)
+	range = malloc(sizeof(int) * (max - min + 1));
+	if (range == NULL)
 	{
 		free(range);
-        return (NULL);
+		return (NULL);
 	}
 
-    for (i = min; i <= max; i++)
-    {
-        range[count] = i;
+	for (i = min; i <= max; i++)
+	{
+		range[count] = i;
 		count++;
-    }
+	}
 
-    return (range);
+	return (range);
 }
