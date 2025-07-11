4e525de6ad0f86f93a602bab852d9de9a4e52372
Priam <priamdmy@gmail.com>
2025-07-11 11:30:08 +0200

    [betty] removed whitelines

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -21,38 +21,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	while (s1[size1] != '\0')
 	{
 		size1++;
 	}
-
 	while (s2[size2] != '\0')
 	{
 		size2++;
 	}
-
 	if (n >= size2)
 		n = size2;
 
 	string = malloc(size1 + n + 1);
 	if (string == NULL)
 		return (NULL);
-
 	for (i = 0; i < size1; i++)
 	{
 		string[i] = s1[i];
 	}
-
 	for (j = 0; j < n; j++)
 	{
 		string[i + j] = s2[j];
 	}
 
 	string[i + j] = '\0';
-
 	return (string);
 }
