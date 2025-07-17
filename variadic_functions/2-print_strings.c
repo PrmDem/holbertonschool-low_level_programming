45a728b4dee09357b3fd25b62582c3a56267cd1f
Priam <priamdmy@gmail.com>
2025-07-17 09:47:59 +0200

    [fix] moved printf("\n") outside if sep != NULL function

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -30,8 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (separator != NULL && i < (n - 1))
 			printf("%s", separator);
-		else if (separator != NULL && i >= (n - 1))
-			printf("\n");
 	}
+	printf("\n");
 	va_end(strings);
 }
