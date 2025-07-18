25fa02cc699a22ff092e4fdcc807a4fca745d7f4
Priam <priamdmy@gmail.com>
2025-07-18 11:23:41 +0200

    [fix] given va_list input as argument to prototypes

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 
@@ -10,5 +13,22 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
+void print_c(va_list input);
+void print_i(va_list input);
+void print_float(va_list input);
+void print_s(va_list input);
+
+/**
+* struct format_base - input of variable types
+* @flag: accepted type of input
+* @print: printf tailored to type
+*
+*/
+
+typedef struct format_base
+{
+	char *flag;
+	void (*print)(va_list input);
+} template;
 
 #endif
