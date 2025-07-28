070c30b131db00fb9d178ddfbdbe6f46a963e05c
Priam <priamdmy@gmail.com>
2025-07-28 13:08:19 +0200

    [first] compil & test successful locally

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node as head of a list
+ * @head: pointer to pointer of new node
+ * @str: data stored in the node
+ *
+ * Return: address of new node
+ * or NULL on failure
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = sizeof(str);
+	new->next = *head;
+
+	*head = new;
+
+	return (*head);
+}
