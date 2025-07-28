43729b26f6238afcc6161a238f2de33652dd972c
Priam <priamdmy@gmail.com>
2025-07-28 10:28:48 +0200

    [first] wrote print_list function, comp & test successful

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		
+		else
+		{
+			printf("[%i] %s\n", current->len, current->str);
+		}
+
+		current = current->next;
+		nodes++;
+	}
+	
+	return (nodes);
+}
