58a1de5ae24736747b45f39d333dc5c2df8fb3f4
Priam <priamdmy@gmail.com>
2025-07-28 18:25:25 +0200

    [fix] changed _strlen for a while loop

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - returns length of a string
- * @s: pointer for first char of string
- *
- * Return: int that is length of string
- */
-
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (s[length] != '\0')
-		length++;
-
-	return (length);
-}
-
-/**
  * add_node - adds a new node as head of a list
  * @head: pointer to pointer of new node
  * @str: data stored in the node
@@ -29,6 +12,12 @@ int _strlen(char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	unsigned int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -38,7 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new->str = strdup(str);
-	new->len = _strlen(str);
+	new->len = length;
 	new->next = *head;
 
 	*head = new;
