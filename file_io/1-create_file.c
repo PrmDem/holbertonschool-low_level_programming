efb14e10e759e044879ad222f5cebcbcc4914d81
Priam <priamdmy@gmail.com>
2025-07-31 18:21:28 +0200

    [first] compil & test successful locally

---
+++ file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: string to write to file
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t count;
+	ssize_t written = 0;
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (0);
+
+	while (text_content[count] != '\0')
+	{
+		count++;
+	}
+
+	written = write(fd, text_content, count);
+
+	close(fd);
+
+	if (written == -1)
+		return (-1);
+	else
+		return (1);
+}
