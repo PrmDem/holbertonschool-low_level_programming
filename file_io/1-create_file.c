b8df5266ae1315aa52eefa0bdce52a35106a3c9f
Priam <priamdmy@gmail.com>
2025-07-31 18:37:58 +0200

    [fix] separated open & creat, no 0600 if file exists

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,10 +15,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_TRUNC);
 
 	if (fd == -1)
-		return (0);
+	{
+		fd = open(filename, O_RDWR | O_CREAT, 0600);
+
+		if (fd == -1)
+			return (-1);
+	}
 
 	while (text_content[count] != '\0')
 	{
