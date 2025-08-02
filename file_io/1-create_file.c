9100f142b45c64bbe34a17860bb82942bec8c641
Priam <priamdmy@gmail.com>
2025-08-02 12:39:47 +0200

    [fix] moved write fnct to if fd == -1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,30 +15,23 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDONLY | O_TRUNC);
+	fd = open(filename, O_RDWR | O_TRUNC);
 	if (fd == -1)
 	{
 		fd = open(filename, O_RDWR | O_CREAT, 0600);
 		if (fd == -1)
 			return (-1);
-	}
-	else
-	{
-		close(fd);
-		return (1);
-	}
 
-	while (text_content[count] != '\0')
-	{
-		count++;
-	}
-
-	if (text_content != NULL)
-	{
-		written = write(fd, text_content, count);
-		if (written == -1)
+		while (text_content[count] != '\0')
 		{
-			return (-1);
+			count++;
+		}
+
+		if (text_content != NULL)
+		{
+			written = write(fd, text_content, count);
+			if (written == -1)
+				return (-1);
 		}
 	}
 
