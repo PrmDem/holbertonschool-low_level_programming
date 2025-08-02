9ac6ce4f2a4032ff31de383538c754230bddef1c
Priam <priamdmy@gmail.com>
2025-08-02 14:32:21 +0200

    [first] local tests successful

---
+++ file_io/3-cp.c
@@ -0,0 +1,91 @@
+#include "main.h"
+
+/**
+ * copy_filetofile - copies contents of a file to another
+ * @filename1: file to copy from
+ * @filename2: file to copy to
+ *
+ * Return: 1 on success,-1 on failure.
+ */
+int copy_filetofile(const char *filename1, const char *filename2)
+{
+	int fd1, fd2;
+	int bread, copy, cl;
+	char *buffer;
+	size_t contlen = 0;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	fd1 = open(filename1, O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename1);
+		exit(98);
+	}
+
+	bread = read(fd1, buffer, 1024);
+	if (bread == -1)
+		return (-1);
+
+	fd2 = open(filename2, O_RDWR | O_TRUNC);
+	if (fd2 == -1)
+	{
+		fd2 = open(filename2, O_RDWR | O_CREAT, 0664);
+		if (fd2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename2);
+		}
+
+		while (buffer[contlen] != '\0')
+		{
+			contlen++;
+		}
+
+		if (buffer != NULL)
+		{
+			copy = write(fd2, buffer, contlen);
+			if (copy == -1)
+				return (-1);
+		}
+	}
+
+	cl = close(fd1);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1);
+		exit(100);
+	}
+
+	cl = close(fd2);
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2);
+		exit(100);
+	}
+
+	return (1);
+}
+
+/**
+ * main - to get our program to work
+ * @ac: number of arguments given
+ * @av: arguments passed to stdin
+ *
+ * Return: always 0.
+ */
+int main(int ac, char **av)
+{
+    int res;
+
+    if (ac != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
+
+    res = copy_filetofile(av[1], av[2]);
+	if (res == -1)
+		return (-1);
+
+    return (0);
+}
