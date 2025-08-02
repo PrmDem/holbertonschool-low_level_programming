7a7206da2e488405175e9b88477bcdd2066ed54f
Priam <priamdmy@gmail.com>
2025-08-02 14:36:45 +0200

    [update] added prototype for copy_filetofile function

--- file_io/main.h
+++ file_io/main.h
@@ -12,5 +12,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int copy_filetofile(const char *filename1, const char *filename2);
 
 #endif
