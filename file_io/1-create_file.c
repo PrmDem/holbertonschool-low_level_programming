ed6ed72aab24ed3a3a3ce23d2e088949583cf0f1
Priam <priamdmy@gmail.com>
2025-08-02 12:34:12 +0200

    [fix] set open rdonly then added if cond in case file exists

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,14 +15,17 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_TRUNC);
-
+	fd = open(filename, O_RDONLY | O_TRUNC);
 	if (fd == -1)
 	{
 		fd = open(filename, O_RDWR | O_CREAT, 0600);
-
 		if (fd == -1)
 			return (-1);
+	}
+	else
+	{
+		close(fd);
+		return (1);
 	}
 
 	while (text_content[count] != '\0')
@@ -31,12 +34,14 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	if (text_content != NULL)
+	{
 		written = write(fd, text_content, count);
+		if (written == -1)
+		{
+			return (-1);
+		}
+	}
 
 	close(fd);
-
-	if (written == -1)
-		return (-1);
-
 	return (1);
 }
