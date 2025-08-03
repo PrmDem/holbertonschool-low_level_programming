b831e3afda18c692ffcbbdeee126d71f33882951
Priam <priamdmy@gmail.com>
2025-08-03 20:25:38 +0200

    [fix] local test successful

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves value at given key
+ * @ht: hash table to look into
+ * @key: key to get value from
+ * Return: value found in key
+ * or NULL if not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+		return (NULL);
+	else
+		return (ht->array[index]->value);
+}
