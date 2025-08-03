517c377f9101f42c364a27b335230c8886e21551
Priam <priamdmy@gmail.com>
2025-08-03 17:08:24 +0200

    [fix] if (*key) == \0 -> if key == NULL

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL)
 		return (0);
 
-	if ((*key) == '\0')
+	if (key == NULL)
 		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
